7a01da735b459c767a0ab8a4fd772a1f82a7ba53
ankurbohra04 <ankurbohra04@gmail.com>
2021-05-05 21:20:29 +0530

    feat: add scheduler activity linker

---
+++ src/linker.py
@@ -0,0 +1,22 @@
+from threading import Timer
+
+from scheduler import Scheduler
+
+# SCHEDULER LINKS
+
+scheduler = Scheduler()
+scheduler.start()
+
+def changeSchedulerActivity(action, timeToLast=-1, callback=None):
+    '''Change the scheduler's activity
+
+    Args:
+        newAction(string): The action to perform on the scheduler. "Pause" or "Resume".
+        timeToLast(float, optional): No. of seconds to change to newAcitivty for before reverting.
+    '''
+    if hasattr(scheduler, action):
+        getattr(scheduler, action)(timeToLast)
+        if callback:
+            Timer(timeToLast, callback)
+    else:
+        raise AttributeError("No such action: %s" % action)
